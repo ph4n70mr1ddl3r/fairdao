86688b1b7ee70f1ee48971fb5f418a521e7cb733
ph4n70mr1ddl3r <105593742+ph4n70mr1ddl3r@users.noreply.github.com>
2025-11-22 20:13:14 +0800

    start with the whitepaper

--- ui/src/wagmi.js
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, optimism, arbitrum, sepolia } from 'wagmi/chains';
-
-export const config = getDefaultConfig({
-    appName: 'FairDAO',
-    projectId: 'YOUR_PROJECT_ID', // TODO: Replace with env var or user input
-    chains: [mainnet, optimism, arbitrum, sepolia],
-    ssr: false, // Client-side only for now
-});
